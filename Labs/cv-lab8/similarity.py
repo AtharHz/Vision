01df5b9dae978f3b6e040ec0526dd7911dc5755d
AtharHz <100313374+AtharHz@users.noreply.github.com>
2024-08-25 18:40:06 +0330

    Add files via upload

---
+++ Labs/cv-lab8/similarity.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+
+I = cv2.imread('karimi.jpg')
+
+tx = 100
+ty = 60
+
+th = 20  # angle of rotation (degrees)
+th *= np.pi / 180  # convert to radians
+
+s = 0.6  # scale factor
+
+M = np.array([[s*np.cos(th), -s*np.sin(th), tx],
+              [s*np.sin(th), s*np.cos(th), ty]])
+
+output_size = (I.shape[1], I.shape[0])
+# output_size = (int(1.5*s*I.shape[1]), int(1.5*s*I.shape[0]))
+J = cv2.warpAffine(I,M,  output_size)
+
+cv2.imshow('I',I)
+cv2.waitKey(0)
+
+cv2.imshow('J',J)
+cv2.waitKey(0)
---
+++ Labs/cv-lab8/task2.py
@@ -0,0 +1,32 @@
+import numpy as np
+import cv2
+
+I = cv2.imread('sign.jpg')
+
+p1 = (135,105)
+p2 = (331,143)
+p3 = (356,292)
+p4 = (136,290)
+
+points1 = np.array([p1,p2,p3,p4], dtype=np.float32)
+
+n = 480
+m = 320
+output_size = (n,m)
+print(output_size)
+
+# J = np.zeros((m,n)) # delete this!!
+
+# mark corners of the plate in image I
+for i in range(4):
+    cv2.circle(I, (int(points1[i,0]), int(points1[i,1])), 5, [0,0,255],2)
+
+points2=np.array([(0,0),(n,0),(n,m),(0,m)]).astype(np.float32)
+H = cv2.getPerspectiveTransform(points1, points2)
+J = cv2.warpPerspective(I,H,  output_size)
+
+cv2.imshow('I', I);
+cv2.waitKey(0)
+
+cv2.imshow('J', J);
+cv2.waitKey(0)
